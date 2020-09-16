c2c4ce3e1948ea97a7283a7ab4c2157de5aa86fc
Nicole Walsh <nmwalsh92@gmail.com>
2020-09-15 19:28:56 -0700

    draft with heroku

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propertymanagementlist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propertymanagementlist", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // Start the API server
 app.listen(PORT, function() {
